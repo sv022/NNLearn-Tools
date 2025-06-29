7df06990a85aa94848f95e4991346f6faac61624
sv022 <ssvatkin04@gmail.com>
2025-06-29 15:15:25 +0300

    chore: fix grayscale to hex conversion

--- src/utils/grayscaleToHex.ts
+++ src/utils/grayscaleToHex.ts
@@ -1,6 +1,13 @@
 export default function grayscaleToHex(grayscaleValue: number) {
   // Ensure the grayscale value is within the valid range (0-255)
 
+  if (grayscaleValue < 0) {
+    return '#000000'
+  }
+  if (grayscaleValue > 1) {
+    return '#FFFFFF'
+  }
+
   let hex = Math.round(grayscaleValue * 255).toString(16)
 
   if (hex.length === 1) {
