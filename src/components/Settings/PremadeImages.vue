5c7d148c758536cfb1193a1b44eebce8423e6bb7
sv022 <ssvatkin04@gmail.com>
2025-06-30 13:50:13 +0300

    chore: update random pixels icon image

--- src/components/Settings/PremadeImages.vue
+++ src/components/Settings/PremadeImages.vue
@@ -25,7 +25,7 @@
     <div class="p-2">
         <Label>Example images</Label>
         <div class="flex p-2 flex-wrap space-y-2">
-            <img width="80" src="@/assets/image_none.png" alt="0" @click="setImage(0)">
+            <img width="80" src="@/assets/image_random.png" alt="0" @click="setImage(0)">
             <img width="80" src="@/assets/image_0.png" alt="0" @click="setImage(1)">
             <img width="80" src="@/assets/image_2.png" alt="0" @click="setImage(2)">
             <img width="80" src="@/assets/image_1.png" alt="0" @click="setImage(3)">
