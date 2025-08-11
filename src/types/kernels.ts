89701493369538d52c3dc8cd5d3b5d68858a741b
sv022 <ssvatkin04@gmail.com>
2025-08-11 19:34:31 +0300

    feat: add 5x5 blur kernels

--- src/types/kernels.ts
+++ src/types/kernels.ts
@@ -1,6 +1,8 @@
 export enum Kernels {
   blur3x3 = 'blur3x3',
+  blur5x5 = 'blur5x5',
   gaussianBlur3x3 = 'gaussianBlur3x3',
+  gaussianBlur5x5 = 'gaussianBlur5x5',
   sharpen = 'sharpen',
   edgeDetection = 'edgeDetection',
   sobelHorizontal = 'sobelHorizontal',
--- src/utils/kernelNames.ts
+++ src/utils/kernelNames.ts
@@ -2,7 +2,9 @@ import { Kernels } from '@/types/kernels'
 
 export const kernelNameMap = {
   [Kernels.blur3x3]: 'Blur 3x3',
+  [Kernels.blur5x5]: 'Blur 5x5',
   [Kernels.gaussianBlur3x3]: 'Gaussian Blur 3x3',
+  [Kernels.gaussianBlur5x5]: 'Gaussian Blur 5x5',
   [Kernels.sharpen]: 'Sharpen',
   [Kernels.edgeDetection]: 'Edge Detection',
   [Kernels.sobelHorizontal]: 'Sobel Horizontal',
