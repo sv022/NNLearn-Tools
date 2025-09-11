55806a81f0be62859503edd6287ea7320ae5d306
sv022 <ssvatkin04@gmail.com>
2025-09-11 11:00:58 +0300

    chore: update navigation

--- src/components/HeaderLogo.vue
+++ src/components/HeaderLogo.vue
@@ -1,9 +1,11 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
 <template>
-
-    <div class="flex items-center md:p-4">
-        <h1 class="text-2xl font-bold text-nowrap">
-            NNLearn Tools
-        </h1>
-    </div>
-
+  <div class="flex items-center md:p-4">
+    <RouterLink to="/">
+      <h1 class="text-2xl font-bold text-nowrap">NNLearn Tools</h1>
+    </RouterLink>
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,25 @@
-<script setup
-  lang="ts">
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <main>
     <div class="flex flex-col items-center justify-center p-2 md:p-0 space-y-10">
       <h1>
-        <span class="text-4xl font-bold text-nowrap">
-          Available tools
-        </span>
+        <span class="text-4xl font-bold text-nowrap"> Available tools </span>
       </h1>
-      <a href="/conv2d">
+      <RouterLink to="/conv2d">
         <div class="border p-2 border-gray-500 rounded-md">
-          <img src="../assets/homepage_demo2.png" alt="demo" width="800">
-          <h2 class="text-2xl font-bold text-center">
-            Convolution 2D Visualizer
-          </h2>
+          <img src="../assets/homepage_demo2.png" alt="demo" width="800" />
+          <h2 class="text-2xl font-bold text-center">Convolution Grayscale Visualizer</h2>
         </div>
-      </a>
+      </RouterLink>
+      <RouterLink to="/convrgb">
+        <div class="border p-2 border-gray-500 rounded-md">
+          <img src="../assets/homepage_demo3.png" alt="demo" width="800" />
+          <h2 class="text-2xl font-bold text-center">Convolution RGB Visualizer</h2>
+        </div>
+      </RouterLink>
     </div>
   </main>
 </template>
